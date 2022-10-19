78417e662fb96003ab963d3a4edac602e2e44780
kartik0009 <89532854+kartik0009@users.noreply.github.com>
2022-10-20 00:15:50 +0530

    added codec run

--- 39_string_match_where_one_string_have_wild_char.cpp
+++ 39_string_match_where_one_string_have_wild_char.cpp
@@ -77,6 +77,51 @@ bool isMatch(string s, string p)
     }
 }
 
+ bool isMatch(string s, string p)
+{void recur(vector<string>& ans, string s, int l, int r)
+{
+    // Base case
+    if (l == r) ans.push_back(s);
+    else
+    {
+        // Permutations made
+        for (int i = l; i <= r; i++)
+        {
+
+            // Swapping done
+            swap(a[l], a[i]);
+
+            // Recursion called
+            permute(a, l + 1, r);
+
+            //backtrack
+            swap(a[l], a[i]);
+        }
+    }
+}
+bool isMatch(string s, string p)
+{void recur(vector<string>& ans, string s, int l, int r)
+{
+    // Base case
+    if (l == r) ans.push_back(s);
+    else
+    {
+        // Permutations made
+        for (int i = l; i <= r; i++)
+        {
+
+            // Swapping done
+            swap(a[l], a[i]);
+
+            // Recursion called
+            permute(a, l + 1, r);
+
+            //backtrack
+            swap(a[l], a[i]);
+        }
+    }
+}
+
     dp.clear();
 
     // resize the dp array
